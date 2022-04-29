a2608defce7f7803a040c09313999d4e95232c75
melvinsjostrand <mesj04001@utb.vaxjo.se>
2022-04-30 01:44:00 +0200

    fågel kan hoppa med mouse1 och space..
    fågel kan hoppa med mouse1 och space. gav fågel gravity

---
+++ Assets/Scripts/bird2.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bird2 : MonoBehaviour{
+    private const float JUMP_AMOUNT = 100f;
+
+    private Rigidbody2D birdRigidbody2D;
+
+    private void Awake(){
+        birdRigidbody2D = GetComponent<Rigidbody2D>();
+    }
+    private void Update() {
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0)) {
+            Jump();
+        }
+    }
+    private void Jump() {
+        birdRigidbody2D.velocity = Vector2.up * JUMP_AMOUNT;
+    }
+    
+}
